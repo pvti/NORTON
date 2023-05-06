e2ccaccd74743872f3b7762931e94cd4267e7355
pvtien96 <pvtien96@gmail.com>
2023-05-06 19:46:30 +0200

    Compute complexity with ptflops

--- complexity.py
+++ complexity.py
@@ -1,9 +1,9 @@
 import argparse
 import torch
-import torch.nn as nn
-from torchprofile import profile_macs
 from models.cifar10.vgg import vgg_16_bn
+from models.cifar10.resnet import resnet_56
 import utils.common as utils
+from ptflops import get_model_complexity_info
 
 
 def parse_args():
@@ -19,40 +19,17 @@ def parse_args():
     return parser.parse_args()
 
 
-def get_num_parameters(model: nn.Module, count_nonzero_only=False) -> int:
-    """
-    calculate the total number of parameters of model
-    :param count_nonzero_only: only count nonzero weights
-    """
-    num_counted_elements = 0
-    for param in model.parameters():
-        if count_nonzero_only:
-            num_counted_elements += param.count_nonzero()
-        else:
-            num_counted_elements += param.numel()
-
-    return num_counted_elements
-
-
-def get_model_macs(model, inputs) -> int:
-
-    return profile_macs(model, inputs)
-
-
 if __name__ == '__main__':
     args = parse_args()
 
-    original_model = vgg_16_bn().cuda()
-    original_params = get_num_parameters(original_model)
-    print('original_params = ', original_params)
+    with torch.cuda.device(0):
+        compress_rate = utils.get_cpr(args.compress_rate)
+        model = eval(args.arch)(compress_rate, args.rank)
 
-    compress_rate = utils.get_cpr(args.compress_rate)
-
-    model = vgg_16_bn(compress_rate, args.rank).cuda()
-    print(model)
-    params = get_num_parameters(model)
-    reduced = (1-params/original_params)*100
-    dummy_input = torch.randn(1, 3, 32, 32).cuda()
-    # macs = get_model_macs(model, dummy_input)
-    # model(dummy_input)
-    print(f'params = {params}, reduced = {reduced:.2f} %')
+        macs, params = get_model_complexity_info(model,
+                                                 (3, 32, 32),
+                                                 as_strings=True,
+                                                 print_per_layer_stat=True,
+                                                 verbose=True)
+        print('{:<30}  {:<8}'.format('Computational complexity: ', macs))
+        print('{:<30}  {:<8}'.format('Number of parameters: ', params))

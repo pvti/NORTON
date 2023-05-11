d92f47766dca4d14fe0792670bd366d5f5dd34a5
tien <pvtien96@gmail.com>
2023-05-11 18:03:07 +0200

    Add scripts to prune decomposed vgg

---
+++ scripts/prune/3.sh
@@ -0,0 +1,4 @@
+for cpr in [0.05]*7+[0.2]*6 [0.2]*7+[0.5]*6 [0.25]*7+[0.75]*6
+do
+    python prune.py --ckpt checkpoint/cifar10/decomposed/vgg/vgg_16_bn_[0.]*100_3.pt --job_dir results/prune -r 3 -cpr $cpr
+done
---
+++ scripts/prune/prune.sh
@@ -0,0 +1,7 @@
+for rank in 1 2 3 4 5 6 7 8
+do
+    for cpr in [0.05]*7+[0.2]*6 [0.2]*7+[0.5]*6 [0.25]*7+[0.75]*6
+    do
+        python prune.py --ckpt checkpoint/cifar10/decomposed/vgg/vgg_16_bn_[0.]*100_$rank.pt --job_dir results/prune -r $rank -cpr $cpr
+    done
+done

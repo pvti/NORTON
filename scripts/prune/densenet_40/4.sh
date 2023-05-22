d30b7c0981e041579c5a8f639692153c6417da07
pvtien96 <pvtien96@gmail.com>
2023-05-22 09:44:00 +0200

    Add scripts to prune resnet56 and densenet40

---
+++ scripts/prune/densenet_40/4.sh
@@ -0,0 +1,4 @@
+for cpr in [0.]+[0.08]*6+[0.09]*6+[0.08]*26 [0.]+[0.2]*12+[0.]+[0.2]*12+[0.]+[0.2]*12 [0.]+[0.3]*12+[0.1]+[0.3]*12+[0.1]+[0.3]*12
+do
+    python prune.py densenet_40 --ckpt checkpoint/cifar10/decomposed/densenet_40/densenet_40_[0.]*100_4.pt --job_dir results/prune -r 4 -cpr $cpr
+done
---
+++ scripts/prune/resnet_56/5.sh
@@ -0,0 +1,4 @@
+for cpr in [0.]+[0.18]*29 [0.]+[0.12]*2+[0.4]*27 [0.]+[0.4]*2+[0.5]*9+[0.6]*9+[0.7]*9
+do
+    python prune.py --arch resnet_56 --ckpt checkpoint/cifar10/decomposed/resnet_56/resnet_56_[0.]*100_5.pt --job_dir results/prune -r 5 -cpr $cpr
+done

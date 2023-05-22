765e8e7978fda231780f51214e0168babcd380b3
pvtien96 <pvtien96@gmail.com>
2023-05-22 16:34:29 +0200

    Fix soft compression rate for densenet40

--- scripts/prune/densenet_40/8.sh
+++ scripts/prune/densenet_40/8.sh
@@ -1,4 +1,4 @@
-for cpr in [0.]+[0.08]*6+[0.09]*6+[0.08]*26 [0.]+[0.2]*12+[0.]+[0.2]*12+[0.]+[0.2]*12 [0.]+[0.3]*12+[0.1]+[0.3]*12+[0.1]+[0.3]*12
+for cpr in [0.]+[0.1]*12+[0.]+[0.1]*12+[0.]+[0.1]*12 [0.]+[0.2]*12+[0.]+[0.2]*12+[0.]+[0.2]*12 [0.]+[0.3]*12+[0.1]+[0.3]*12+[0.1]+[0.3]*12
 do
-    python prune.py densenet_40 --ckpt checkpoint/cifar10/decomposed/densenet_40/densenet_40_[0.]*100_8.pt --job_dir results/prune -r 8 -cpr $cpr
+    python prune.py --arch densenet_40 --ckpt checkpoint/cifar10/decomposed/densenet_40/densenet_40_[0.]*100_8.pt --job_dir results/prune -r 8 -cpr $cpr
 done
